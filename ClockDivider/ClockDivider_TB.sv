c4820bb3c4e6f800f459f0a16ba0ba8133516000
mynordav <sphdav@gmail.com>
2017-06-01 18:02:25 -0500

    Update ClockDivider_TB.sv

--- ClockDivider/ClockDivider_TB.sv
+++ ClockDivider/ClockDivider_TB.sv
@@ -1,17 +1,38 @@
+/****************************************************************************
+DUT name:
+	ClockDivider.sv
+Description:
+	Testing of ClockDivider module with a given input and output frequencies
+Parameters:
+	OUT_FREQUENCY: Output clock frequency to generate
+	IN_FREQUENCY:  Source clock frequency
+Inputs:
+	clk_in: Source clock signal
+	rst: Reset signal. Active low.
+Outputs:
+	clk: Clock signal with the desired frequency.
+Version:
+	1.0
+Autors:
+	Mynor Davila
+	Montserrat De Aguinaga
+Date:
+	01/jun/17
+****************************************************************************/
 
-timeunit 10ms; //It specifies the time unit that all the delay will take in the simulation.
-timeprecision 1ms;// It specifies the resolution in the simulation.
+timeunit 10ms;     //It specifies the time unit that all the delay will take in the simulation.
+timeprecision 1ms; // It specifies the resolution in the simulation.
 
 module ClockDivider_TB;
 
-parameter OUT_FREQUENCY = 1;
-parameter IN_FREQUENCY = 10;
+parameter OUT_FREQUENCY = 1; //The module should generate a 1 Hz clock signal
+parameter IN_FREQUENCY = 10; //The source clock is at 10 Hz
 
- // Input Ports
+// Input Ports
 bit clk_in = 0;
 bit rst;
 	
-  // Output Ports
+// Output Ports
 logic clk; 
 ClockDivider
 #(
@@ -38,5 +59,6 @@ initial begin // reset generator
 	#0 rst = 0;
 	#15 rst = 1;
 end
+/*********************************************************/
 
 endmodule
