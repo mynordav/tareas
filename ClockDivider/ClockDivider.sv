81d6566ce3a91188241413c0a20d52892da26bdd
mynordav <sphdav@gmail.com>
2017-06-01 20:57:55 -0500

    Add files via upload

--- ClockDivider/ClockDivider.sv
+++ ClockDivider/ClockDivider.sv
@@ -1,25 +1,7 @@
-/****************************************************************************
-Module name:
-	ClockDivider.sv
-Description:
-	This module is a parametrizable frequency divider.
-Inputs:
-	Board clock and reset.
-Outputs:
-	Clock divided.
-Version:
-	1.0
-Autors:
-	Mynor Davila
-	Montserrat De Aguinaga
-Date:
-	1/jun/17
-****************************************************************************/
-
 module ClockDivider
 #(
-	parameter OUT_FREQUENCY = 100,		/* Desire output frequency */
-	parameter IN_FREQUENCY = 50000000	/* Input frequency */
+	parameter OUT_FREQUENCY = 100,
+	parameter IN_FREQUENCY = 50000000
 )
 (
 	input clk_in,
@@ -28,27 +10,37 @@ module ClockDivider
 	output reg clk
 );
 
-localparam MAX_COUNT = IN_FREQUENCY/(2*OUT_FREQUENCY);	/* Max value for half the period of the designed signal */
+localparam MAX_COUNT = IN_FREQUENCY/(2*OUT_FREQUENCY);
+localparam COUNTER_WIDTH = CeilLog2(MAX_COUNT);
 
-integer counter = 0;
+logic [COUNTER_WIDTH-1:0] counter;
 
 always_ff@(posedge clk_in or negedge rst) begin
-	if (rst == 1'b0) begin				/* If reset is pressed, clock and counter return to 0 */
+	if (rst == 1'b0) begin
 		clk <= 1'b0;
 		counter <= 0;
 	end
 	else begin 
-		if(counter == MAX_COUNT - 1) begin	/* If counter reached the max value, togle the output */
-			clk <= ~clk;			/* signal and restart counter */
+		if(counter == MAX_COUNT - 1) begin
+			clk <= ~clk;
 			counter <= 0;
 		end
 		else
-			counter++;			/* If counter hasnt reached max value, increase it */ 
+			counter++;
 	end
 end
 
 
 endmodule
 
-
+/*Log Function*/
+function integer CeilLog2;
+ input integer data;
+ integer i,result;
+ begin
+	 for(i=0; 2**i < data; i=i+1)
+		 result = i + 1;
+	 CeilLog2 = result;
+ end
+endfunction
 
